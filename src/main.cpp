03f3c721be5d1c931f51421df8ef7bce91b88eca
Colin Harris <col.w.harris@gmail.com>
2015-02-22 23:09:36 +0800

    [WIP] - Trying out the rtos libraries. No really sure what I'm doing at this point.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "mbed.h"
 #include "receiver_config.h"
 #include "pwm_receiver.h"
+#include "rtos.h"
 
 DigitalOut led1(LED1);
 DigitalOut led2(LED2);
@@ -40,70 +41,57 @@ void init(void)
 
 }
 
-void main_loop(void)
+void usb_loop(void const *args)
 {
-  if( console.readable() ) {
-
-    led1 = !led1;
-
-    char char1 = console.getc();
-    char char2 = console.getc();
-
-    if( char1 == 'R' )
+  while(true)
+  {
+    if( console.readable() )
     {
-      if( char2 == 'X' )
+      led1 = !led1;
+      char char1 = console.getc();
+      char char2 = console.getc();
+
+      if( char1 == 'R' && char2 == 'X' )
       {
-        console.printf(
-          "CH1:%4lu,CH2:%4lu,CH3:%4lu,CH4:%4lu;",
-          rx.readThrottle(), rx.readYaw(), rx.readPitch(), rx.readRoll(), 0, 0
-        );
+          // Output the scaled values
+          console.printf(
+            "CH1:%4lu,CH2:%4lu,CH3:%4lu,CH4:%4lu,CH5:%4lu,CH6:%4lu;",
+            rx.readThrottle(), rx.readYaw(), rx.readPitch(), rx.readRoll(), 0, 0
+          );
       }
-      else
+      else if( char1 == 'R' && char2 == 'R' )
       {
-        console.printf(
-          "CH1:%4lu,CH2:%4lu,CH3:%4lu,CH4:%4lu;",
-          rx.readChannel(1), rx.readChannel(2), rx.readChannel(3), rx.readChannel(4)
-        );
+          // Output the raw values
+          console.printf(
+            "CH1:%4lu,CH2:%4lu,CH3:%4lu,CH4:%4lu,CH5:%4lu,CH6:%4lu;",
+            rx.readChannel(1), rx.readChannel(2), rx.readChannel(3), rx.readChannel(4), 0, 0
+          );
       }
+
     }
-
+    Thread::wait(20); // 50Hz / 20ms - Currently matching the Rx input speed.
   }
+}
 
+void main_loop()
+{
   int throttle = rx.readThrottle();
   float output = ((float)throttle/1000000)+0.001;
   motor1.pulsewidth(output);
   motor2.pulsewidth(output);
   motor3.pulsewidth(output);
   motor4.pulsewidth(output);
-
-  wait(0.005);
-}
-
-void printChannelConfig(ChannelConfig config)
-{
-  console.printf("Min Value: %4lu ", config.minValue);
-  console.printf("Max Value: %4lu ", config.maxValue);
-  console.printf("Reverse: %d ", config.reverse);
-  console.printf("Channel: %lu\n", config.channel);
-}
-
-void printChannelConfigs()
-{
-  console.printf("Throttle Config: ");
-  printChannelConfig(rxConfig.throttleConfig());
-  console.printf("Yaw Config: ");
-  printChannelConfig(rxConfig.yawConfig());
-  console.printf("Pitch Config: ");
-  printChannelConfig(rxConfig.pitchConfig());
-  console.printf("Roll Config: ");
-  printChannelConfig(rxConfig.rollConfig());
 }
 
 int main()
 {
     init();
-    while (1) {
+
+    Thread usb_thread(usb_loop);
+    usb_thread.set_priority(osPriorityLow);
+
+    while(true) {
         main_loop();
-        wait(0.005);
+        Thread::wait(20); // 50Hz / 20ms - Currently matching the Rx input speed.
     }
 }
